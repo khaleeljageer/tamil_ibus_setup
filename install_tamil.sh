52fd24352b8c80c7e26cf6e5b4d8c183c4e5069c
Khaleel Jageer <jskcse4@gmail.com>
2021-02-11 08:44:25 +0530

    t99/phonetic handle added

--- install_tamil.sh
+++ install_tamil.sh
@@ -7,13 +7,13 @@ sudo apt-get update -y
 sudo apt-get install ibus ibus-m17n -y
 
 #Installing ibus-gtk package
-# sudo apt-get install ibus-gtk -y
+sudo apt-get install ibus-gtk -y
 
 #Installing ibus-gtk3 package
-# sudo apt-get install ibus-gtk3 -y
+sudo apt-get install ibus-gtk3 -y
 
 #Installing ibus-qt4 package
-# sudo apt-get install ibus-qt4 -y
+sudo apt-get install ibus-qt4 -y
 
 #Starting ibus session
 ibus-daemon
@@ -21,10 +21,14 @@ ibus-daemon
 #Setting ibus as a default input method
 im-config -n ibus
 
-# This will set both Indian-English Keyboard and Phonetic Keyboard as an Input Source.
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'in+eng'), ('ibus', 'm17n:ta:phonetic')]"
-
-# This will set both Indian-English Keyboard and Tamil'99 as an Input Source.
-# gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'in+eng'), ('ibus', 'm17n:ta:tamil99')]"
+# This will set both Indian-English Keyboard and selected optional keyboard as an Input Source.
+# Default is Phonetic
+if [[$1 = "-tamil99"]]; then
+    echo "Tamil99 keyboard selected"
+    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'in+eng'), ('ibus', 'm17n:ta:tamil99')]"
+else
+    echo "Default keyboard selected(Phonetic)"
+    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'in+eng'), ('ibus', 'm17n:ta:phonetic')]"
+fi
 
 echo "Please \"Restart\" to make the changes effect"
